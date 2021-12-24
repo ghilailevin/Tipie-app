fb9ba28a596f9c4ab3f724f29aa72804dc7228e8
ghilai levin <ghilailevin97@gmail.com>
2021-12-23 21:47:19 -0300

    tipie

---
+++ Client/client/src/componentes/Home.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import DataTable from "react-data-table-component"
+import 'bootstrap/dist/css/bootstrap.min'
+
+const columns = [
+    {
+        name= "ID",
+        age= "ID",
+        sector= "text",
+        email= "true"
+    },
+    {
+        name= "ID",
+        age= "ID",
+        sector= "text",
+        email= "true"
+    },
+    {
+        name= "ID",
+        age= "ID",
+        sector= "text",
+        email= "true"
+    },
+    {
+        name= "ID",
+        age= "ID",
+        sector= "text",
+        email= "true"
+    },
+    {
+        name= "ID",
+        age= "ID",
+        sector= "text",
+        email= "true"
+    }
+]
+
+const Home = () => {
+    return ( 
+        <div>
+        </div>
+    );
+}
+ 
+export default Home;
